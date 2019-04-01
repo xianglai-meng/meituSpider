e46f3ab48cc5e515a8e2c05421d322bcdd872ae1
Joey <463901421@qq.com>
2019-04-02 00:21:38 +0800

    before success

--- GetGirlPictures.py
+++ GetGirlPictures.py
@@ -2,11 +2,12 @@
 
 from urllib import request
 import re
+from bs4 import BeautifulSoup
 
 fileName = 0
 historyList=[]
 
-def getPicture(url):
+def getHtml(url):
     opener=request.build_opener()
     chaper_url= url
     #chaper_url='https://www.ishsh.com/wp-content/uploads/2018/04/hk1-1.jpg'
@@ -15,27 +16,52 @@ def getPicture(url):
     request.install_opener(opener)
     respone = request.urlopen(chaper_url)
     html= respone.read().decode('utf-8') 
+    return html
+
+def getPicture(html1,html2):
+    html=html1+html2
+    # opener=request.build_opener()
+    # chaper_url= url
+    # #chaper_url='https://www.ishsh.com/wp-content/uploads/2018/04/hk1-1.jpg'
+    # #opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36')]
+    # opener.addheaders=[('User-Agent','Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0')]
+    # request.install_opener(opener)
+    # respone = request.urlopen(chaper_url)
+    # html= respone.read().decode('utf-8') 
     #request.urlretrieve(url=chaper_url,filename='/home/joey/download/1.jpg')
+    htmlcontent = getHtml(html)
+    currentId= html2.replace('.html','')
+    retxt ='<a class.*?href="{}.*?html".*?<img.*?(src=)?.*?</a>'.format(currentId)
 
-    pattern =re.compile('(?<=<img .*?src=")https?.*?(?=")')
+    patternC=re.compile(retxt)
+    img = patternC.findall(htmlcontent)
+    print(htmlcontent)
+    print(img)
+    # pattern =re.compile('(?<=<img.*?src=")https?.*?(?=")')
+    pattern =re.compile('<img.*?(src=)?"(https?.*?.jpg)"')
 
-    imgList = pattern.findall(html)
+    imgList = pattern.findall(htmlcontent)
+ 
     # 判断是否有SRC=
     global fileName 
     global historyList 
     #imgList.remove(imgList[1])
     try:
         for img in imgList:
-        # fileName+=1       
-            #request.urlretrieve(url=str(img),filename= '/home/joey/download/{}.jpg'.format(fileName))
-            #print(img)
+            # print(img[1])
+            # fileName+=1       
+            # request.urlretrieve(url=str(img[1]),filename= '/home/joey/download/{}.jpg'.format(fileName))
+            # #print(img)
 
-            patterSub= re.findall('[^\x00-\xff]',img)
-            if  len(patterSub)>0:
-                continue
-            index = img.find('src')
-            if index>0:
-                imgUrl = img.split('src=')[1]
+            # patterSub= re.findall('[^\x00-\xff]',img)
+            # if  len(patterSub)>0:
+            #     continue
+            #index = img.find('src')
+            
+            #if index>0:
+            #imgUrl = img.split('src=')[1]
+            if len(img)>1:
+                imgUrl = img[1]
                 fileName+=1
                 if  len(historyList)>0:
                     if  imgUrl not in historyList:
@@ -50,45 +76,55 @@ def getPicture(url):
                     print(imgUrl)
                     print('*'*50)
                     print(historyList)        
-            else:    
-                fileName+=1
-                if  len(historyList)>0:
-                    if  img not in historyList:
-                        request.urlretrieve(url=img,filename= '/home/joey/download/{}.jpg'.format(fileName))
-                        print(img)
-                        print('*'*50)
-                        print(historyList)
+            # else:    
+            #     fileName+=1
+            #     if  len(historyList)>0:
+            #         if  img not in historyList:
+            #             request.urlretrieve(url=img,filename= '/home/joey/download/{}.jpg'.format(fileName))
+            #             print(img)
+            #             print('*'*50)
+            #             print(historyList)
                         
     except IOError as identifier:
         pass
 
+def getAllUrlOnePage(html,mainUrl):
+    htmlcontent =getHtml(html)
+    pattern =re.compile('href="(/\d{2,10}\.html)"')
+    imgList = pattern.findall(htmlcontent)
+    print(htmlcontent)
+    return imgList
 
-def getAllUrl(html,mainUrl):
-	opener=request.build_opener()
-	chaper_url=html
-	opener.addheaders=[('User-Agent','Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0')]
-	request.install_opener(opener)
-	#1\返回对象
-	respone = request.urlopen(chaper_url)
-	html= respone.read().decode('utf-8') 
-	#html= respone.read()
+# def getAllUrlOnePage(html,mainUrl):
+#     htmlcontent =getHtml(html)
+#     pattern =re.compile('href="(https?.*ishsh.com.*\.html)')
+#     imgList = pattern.findall(htmlcontent)
+#     print(htmlcontent)
+#     return imgList
 
-	#2、返回html内容
-	#html = requests.get(html).content.decode("utf-8")
+def getAllMainUrl(html):
+    soup = BeautifulSoup(html, 'html.parser')  
+    catalog = soup.find_all('li',{'id':re.compile("menu-item")})
 
-	#print(html)
+    mainUrlList=[]
+    for urllist in catalog:
+        pattern =re.compile('href="(.*)"')
+        urlstr =pattern.findall(str(urllist))
+        if len(urlstr[0])<20:
+            mainUrlList.append(urlstr[0])
 
-	pattern =re.compile('href="(https?.*ishsh.com.*\.html)')
-
-	imgList = pattern.findall(html)
-
-	print(imgList)
-
-	return imgList
+    return mainUrlList
 
 if __name__ == '__main__':
+    mainUrl = "https://www.ishsh.com"
+    htmltxt = getHtml(mainUrl+"/gaoqing")
+
+    mainUrlLists = getAllMainUrl(htmltxt)
+
     urllists = []
-    urllists = getAllUrl('https://www.ishsh.com/','ishsh')
-    for url in urllists:
-        getPicture(url)
+    for mainurl in mainUrlLists:
+        imageUrl = mainUrl+mainurl
+        urllists = getAllUrlOnePage(imageUrl,'ishsh')
+        for url in urllists:
+            getPicture(mainUrl,url)
     
